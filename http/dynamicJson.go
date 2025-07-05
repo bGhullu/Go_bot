055b343c0ac7aed9ff5cd94155d26651962a93f1
bghullu <ghullu.bijay@gmail.com>
2025-07-05 16:50:48 +0545

    GO

--- http/dynamicJson.go
+++ http/dynamicJson.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	jsonStr := `{
+	jsonStr := `[ 
+	{
 		"name": "Alice",
 		"age": 30,
 		"active": true,
@@ -14,25 +15,35 @@ func main() {
 		"details": {
 		"role": "admin"
 		}
-	}`
+	},
+	{
+		"name": "Rice",
+		"age": 40,
+		"active": false,
+		"scores": [10, 5],
+		"details": {
+		"role": "employee"
+		}
+	}
+	]`
 
-	var data map[string]interface{}
+	var data []map[string]interface{}
 	err := json.Unmarshal([]byte(jsonStr), &data)
 	if err != nil {
 		panic(err)
 	}
-
-	name := data["name"].(string)
-	age := data["age"].(float64)
-	active := data["active"].(bool)
-	scores := data["scores"].([]interface{})
-	details := data["details"].(map[string]interface{})
-	role := details["role"].(string)
-
-	fmt.Println("Name:", name)
-	fmt.Println("Age:", age)
-	fmt.Println("Active:", active)
-	fmt.Println("First Score:", scores[0])
-	fmt.Println("Role:", role)
+	for _, info := range data {
+		name := info["name"].(string)
+		age := info["age"].(float64)
+		active := info["active"].(bool)
+		scores := info["scores"].([]interface{})
+		details := info["details"].(map[string]interface{})
+		role := details["role"].(string)
+		fmt.Println("\nName:", name)
+		fmt.Println("Age:", age)
+		fmt.Println("Active:", active)
+		fmt.Println("First Score:", scores[0])
+		fmt.Println("Role:", role)
+	}
 
 }
